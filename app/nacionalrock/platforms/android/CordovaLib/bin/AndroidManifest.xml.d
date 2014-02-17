8e95ced16994362180afd616b90907986460c53d
Rama <radioflow@xicnet.com>
2014-02-17 15:50:22 -0300

    Tools

---
+++ app/nacionalrock/platforms/android/CordovaLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/rama/dev/radioflow/app/nacionalrock/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
+/home/rama/dev/radioflow/app/nacionalrock/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ app/nacionalrock/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/rama/dev/radioflow/app/nacionalrock/platforms/android/bin/AndroidManifest.xml : \
+/home/rama/dev/radioflow/app/nacionalrock/platforms/android/AndroidManifest.xml \
