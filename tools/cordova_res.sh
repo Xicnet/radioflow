928249ab353a6fefe85305b9abb04f8d25229d5c
Ramiro Cosentino <racapp@xicnet.com>
2014-02-17 00:28:58 -0300

    Improve resources script

--- tools/cordova_res.sh
+++ tools/cordova_res.sh
@@ -1,18 +1,27 @@
 #  654  cp /Users/rama/Desktop/dev/nacionalrock.2013/app/nacionalrock/www/res/icon/ios/icon-57.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/HellWorld/Resources/icons/icon.png 
 #  660  cp /Users/rama/Desktop/dev/nacionalrock.2013/app/nacionalrock/platforms/ios/Nacional\ Rock/Resources/splash/Default~iphone.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/HellWorld/Resources/splash/
 
-APP_NAME="HellWorld"
+APP_NAME=$1
+APP_DIR=$2
+APP_ROOT=$3
+RES_SRC=$4
 
-cp -v res_src/icons/icon-57.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/icons/icon.png
-cp -v res_src/icons/icon@2x.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/icons/ 
-cp -v res_src/splash/Default~iphone.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/splash/
-cp -v res_src/splash/Default~iphone.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/splash/Default-568h@2x.png
+APP_NAME="NacionalRock"
+APP_SHORTNAME="nacionalrock"
+APP_DIR="/Users/rama/Desktop/dev/radioflow/app/$APP_SHORTNAME"
+APP_RES="$APP_DIR/platforms/ios/$APP_NAME/Resources"
+RES_SRC="/Users/rama/Desktop/dev/RNA.assets/$APP_SHORTNAME"
 
-cp -v res_src/splash/Default~iphone.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/splash/Default-568h@2x~iphone.png
-cp -v res_src/splash/Default~iphone.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/splash/Default-Landscape@2x~ipad.png
-cp -v res_src/splash/Default~iphone.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/splash/Default-Landscape~ipad.png
-cp -v res_src/splash/Default~iphone.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/splash/Default-Portrait@2x~ipad.png
-cp -v res_src/splash/Default~iphone.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/splash/Default-Portrait~ipad.png
-cp -v res_src/splash/Default~iphone.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/splash/Default@2x~iphone.png
-cp -v res_src/splash/Default~iphone.png /Users/rama/Desktop/dev/cordovatest/hell/platforms/ios/$APP_NAME/Resources/splash/Default~iphone.png
+cp -v $RES_SRC/icons/icon-57.png $APP_RES/icons/icon.png
+cp -v $RES_SRC/icons/icon@2x.png $APP_RES/icons/ 
+cp -v $RES_SRC/splash/Default~iphone.png $APP_RES/splash/
+cp -v $RES_SRC/splash/Default~iphone.png $APP_RES/splash/Default-568h@2x.png
+
+cp -v $RES_SRC/splash/Default~iphone.png $APP_RES/splash/Default-568h@2x~iphone.png
+cp -v $RES_SRC/splash/Default~iphone.png $APP_RES/splash/Default-Landscape@2x~ipad.png
+cp -v $RES_SRC/splash/Default~iphone.png $APP_RES/splash/Default-Landscape~ipad.png
+cp -v $RES_SRC/splash/Default~iphone.png $APP_RES/splash/Default-Portrait@2x~ipad.png
+cp -v $RES_SRC/splash/Default~iphone.png $APP_RES/splash/Default-Portrait~ipad.png
+cp -v $RES_SRC/splash/Default~iphone.png $APP_RES/splash/Default@2x~iphone.png
+cp -v $RES_SRC/splash/Default~iphone.png $APP_RES/splash/Default~iphone.png
 
