bad55f47720f2283a8d77caf76703abec6e36d11
Ramiro Augusto Cosentino <rama@xicnet.com>
2014-02-17 22:01:28 -0300

    Improve tools

--- tools/build_all.sh
+++ tools/build_all.sh
@@ -1,1 +1,7 @@
-make build_all PLATFORM=android RELEASE=release
+#make build_all PLATFORM=android RELEASE=release
+
+bash clone.sh android nacionalfolklorica NacionalFolklorica release 
+bash clone.sh android nacionalclasica NacionalClasica release 
+bash clone.sh android radionacionalam870 RadioNacionalAM870 release 
+bash clone.sh android rae RAE release 
+
