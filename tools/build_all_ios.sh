8eb39c97bf3a5f9dea181b5a2c06d369013ec4e5
Ramiro Cosentino <racapp@xicnet.com>
2014-02-20 16:22:51 -0300

    Add iOS build all script

---
+++ tools/build_all_ios.sh
@@ -0,0 +1,7 @@
+#make build_all PLATFORM=ios RELEASE=release
+
+bash clone.sh ios nacionalfolklorica NacionalFolklorica release 
+bash clone.sh ios nacionalclasica NacionalClasica release 
+bash clone.sh ios radionacionalam870 RadioNacionalAM870 release 
+bash clone.sh ios rae RAE release 
+
