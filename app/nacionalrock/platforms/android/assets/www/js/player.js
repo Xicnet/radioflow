7333e392fc914f6e1357d418b950c7593bdedce0
Ramiro Augusto Cosentino <rama@xicnet.com>
2014-02-20 22:03:22 -0300

    Update android

--- app/nacionalrock/platforms/android/assets/www/js/player.js
+++ app/nacionalrock/platforms/android/assets/www/js/player.js
@@ -1,51 +1,33 @@
-	var myMedia   = null;
-	var isPlaying = false;
-	var streamURL;
+var playButton;
+var myMedia   = null;
+var isPlaying = false;
+var streamURL;
        
-	function audioToggle() {
-		if (!isPlaying) {
-			playAudio();
-		} else {
-			stopAudio();
-		}
-	}
  
-	function playAudio() {
+var mediaAudio = {
+	play: function() {
 		var streamURL = "http://5.9.56.134:8162/;stream.nsv";
-		if(device.platform == "Android") {
-			myMedia = new Media(streamURL, stopAudio, mediaError, mediaStatus);
-			console.log(myMedia);
-			app.addToCal();
-		} else {
-			myMedia = new Audio(streamURL, stopAudio, null);
-		}
+		myMedia = new Media(streamURL, this.stop, mediaError, mediaStatus);
 		myMedia.play();     
-		document.getElementById('playButton').src = "img/pause.png";
 		isPlaying = true; 
+		playButton.src = "img/pause.png";
 		getProgramInfo();
-	}
-
-	function stopAudio() {
-		if(device.platform == "Android") {
-			app.removeNoti();
-		}
-		myMedia.pause();
+		app.addToCal();
+	},
+	stop: function() {
+		app.removeNoti();
 		hideProgramInfo();
+		myMedia.pause();
 		isPlaying = false;
-		document.getElementById('playButton').src = "img/play.png";   
-		//document.getElementById('audio_position').innerHTML = "0.000 sec";
+		playButton.src = "img/play.png";   
 		myMedia.release();
 	}
+}
  
-	function onDeviceReady(){
-		console.log("RNA: Got device ready");
-		//updateMedia();
-	}
+function mediaStatus(e){
+	console.log("RNA Media Status: "+ e);
+}
 
-	function mediaStatus(e){
-		console.log("RNA Media Status: "+ e);
-	}
-
-	function mediaError(error){
-		console.log("RNA Media Error: "+ error);
-	}
+function mediaError(error){
+	console.log("RNA Media Error: "+ error);
+}
