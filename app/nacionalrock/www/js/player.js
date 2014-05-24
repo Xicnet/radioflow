bc63a28659de7da51e6856b3b782866fcbab3913
Rama <radioflow@xicnet.com>
2014-05-24 06:40:04 -0300

    Show buffering... message when play button pressed. Hide buffering... message when playing

--- app/nacionalrock/www/js/player.js
+++ app/nacionalrock/www/js/player.js
@@ -23,7 +23,20 @@ var mediaAudio = {
 }
  
 function mediaStatus(e){
+	/*
+	Media.MEDIA_NONE = 0;
+	Media.MEDIA_STARTING = 1;
+	Media.MEDIA_RUNNING = 2;
+	Media.MEDIA_PAUSED = 3;
+	Media.MEDIA_STOPPED = 4;
+	*/
 	console.log("RNA Media Status: "+ e);
+	if(e==1) {
+		$("#buffering").css("display", "block");
+	}
+	if(e==2) {
+		$("#buffering").css("display", "none");
+	}
 }
 
 function mediaError(error){
