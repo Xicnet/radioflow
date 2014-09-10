a404c59c8dd81eb888e34857154a59f0de9be8b6
Ramiro Cosentino <racapp@xicnet.com>
2014-09-10 19:20:04 -0300

    Added OTA for nacionalrock

--- app/nacionalrock/platforms/ios/www/js/config.js
+++
@@ -1,7 +0,0 @@
-var contentServer = "http://rnadmin.xicnet.com";
-var streamURL     = "http://5.9.56.134:8162/;stream.nsv";
-
-// Make config variables global
-window.server = contentServer;
-window.streamURL = streamURL;
-window.isPlaying = false;
