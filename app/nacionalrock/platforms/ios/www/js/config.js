e1cb0e450e08167f6975497f72f34b21dfed1b3c
Ramiro Cosentino <racapp@xicnet.com>
2014-09-10 21:12:24 -0300

    Update iOS

---
+++ app/nacionalrock/platforms/ios/www/js/config.js
@@ -0,0 +1,11 @@
+var contentServer = "http://rnadmin.xicnet.com";
+var streamURL     = "http://5.9.56.134:8162/;stream.nsv";
+
+// Make config variables global
+window.server = contentServer;
+window.streamURL = streamURL;
+window.facebook = "fb://profile/239488842805946";
+window.twitter = "twitter://twitter.com/nacionalrock937";
+window.web = "http://www.nacionalrock.com/";
+window.email = "contacto@nacionalrock.com";
+window.isPlaying = false;
---
+++ app/nacionalrock/platforms/ios/www/js/index.js
@@ -0,0 +1,236 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+stationName = "nacionalrock";
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+
+	// Override back button
+	document.addEventListener("backbutton", ShowExitDialog, false);
+	$('.play-button').on('tap', onTapPlayHandler);
+	function onTapPlayHandler() {
+		$(this).find('.glyph-icon').addClass('blink');
+	}
+
+	$('.links').on('tap', onTapHandler);
+	function onTapHandler() {
+		$(this).find('.glyph-icon').addClass('blink');
+		setTimeout(function(){ $('div').removeClass('blink');}, 2000);
+	}
+
+	// Dialog box when back button is pressed
+	function ShowExitDialog() {
+		navigator.notification.confirm(
+			("Desea salir?"), // message
+			alertexit, // callback
+			'Nacional Rock', // title
+			['Sí', 'No'] // buttonName
+		);
+	}
+
+	// Call exit function
+	function alertexit(button){
+	        if(button=="1" || button==1)
+        	{
+	            //device.exitApp();
+	            navigator.app.exitApp();
+        	}
+	}
+
+	// Server to fetch config (background image and streamURL) from
+	var url = window.server + "/" + stationName + "/config.json?"+Math.random();
+
+	// Fetch the config
+
+	$.ajaxSetup({
+		timeout: 3000, 
+		retryAfter:7000
+	});
+
+	function getConfig(){
+		$.ajax({
+			url: url,
+			global: false,
+			type: "GET",
+			dataType: "json",
+			async:true,
+		})
+		.success(function(data){
+			//alert('Ajax request worked');
+			appSetup(data);
+		})
+		.error(function(){
+			setTimeout (getConfig, $.ajaxSetup().retryAfter);
+		});
+	}
+
+	function appSetup(config) {
+		// Set streamURL as a global variable to be used by player
+		if (config.streamurl) {
+			window.streamURL = config.streamurl;
+		}
+		if (config.facebook) {
+			window.facebook = config.facebook;
+		}
+		if (config.twitter) {
+			window.twitter = config.twitter;
+		}
+		if (config.web) {
+			window.web = config.web;
+		}
+		if (config.email) {
+			window.email = config.email;
+		}
+		setBackgroundImage(config.image);
+		if (config.logourl) {
+			setLogoImage(config.logourl);
+		}
+               	$("#wrapper").css("display", "block");
+               	$("#loading").css("display", "none");
+	}
+	getConfig();
+
+
+	// Set background image
+	function setBackgroundImage(url) {
+		$("div#wrapper").css({'background-image':"url('"+url+"')"});
+	}
+	function setLogoImage(url) {
+		$(".logo").attr('src', url);
+	}
+	function setStreamURL(url) {
+		if (url) {
+			window.streamURL = url;
+		}
+	}
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    addToCal: function() {
+	var success = function() { console.log("Notification successfully added"); };
+        var error = function(message) { console.log("Oopsie! " + message); };
+        statusbarnotification.createEvent(success, error);
+    },
+
+    removeNoti: function() {
+	var success = function() { console.log("Notification successfully added"); };
+	var success = function(message) { console.log("Remove noti"); };
+	var error = function(message) { console.log("Oopsie! " + message); };
+       	statusbarnotification.removeNotification(success, error);
+    },
+    audioToggle: function() {
+	console.log("............ in audioToggle()");
+	if(device.platform == "iOS") {
+		player = html5audio;
+		console.log("html5audio PLAYER: " + window.streamURL);
+	} else {
+		player = mediaAudio;
+		console.log("mediaPlugin PLAYER: " + window.streamURL);
+	}
+	if (isPlaying || isStarting) {
+		player.stop();
+	} else {
+		player.play();
+	}   
+    },
+    socialLink: function() {
+	    alert($(this));
+    }
+
+};
+
+
+function getProgramInfo()
+{
+	if(window.isPlaying == false) {
+		return;
+	}
+	if(isPlaying) {
+        	var url = window.server + "/" + stationName + "/now_playing.json?"+Math.random();
+        	console.log("getProgramInfo url : " + url);
+	        $.getJSON(url, function(data) {
+	                if(data.name) {
+        	        	$('#name').html(data.name);
+			}
+	                if(data.presenter) {
+	                	$('#presenter').html(data.presenter);
+			}
+	                if(data.show_labels) {
+	                	$('.infopanel-label').css("display", "inline");
+			} else {
+	                	$('.infopanel-label').css("display", "none");
+			}
+	                if(data.image.length > 0) {
+	                	var image = data.image_url;
+				$(".program-image").attr("src", image);
+	                	$(".program-image").css("visibility", "visible");
+			}
+	                $(".program-info").css("visibility", "visible");
+			$(".infopanel-container").css("visibility", "visible");
+	        });
+	}
+}
+
+// Check for program info changes
+var checkInterval = 1;
+var timerUnit = 60000;
+var timerUnit = 6000;
+var interval = setInterval(getProgramInfo, timerUnit * checkInterval);
+
+function hideProgramInfo()
+{
+                $(".infopanel-container").css("visibility", "hidden");
+                $(".program-info").css("visibility", "hidden");
+		$(".program-image").css("visibility", "hidden");
+
+}
+
+
+function buildContact(contact)
+{
+	if(contact.indexOf("http://") == 0) {
+		return contact;
+	} else {
+		return "mailto:" + contact;
+	}
+}
