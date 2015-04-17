87030cb71c3bdd6e7405e0c1f70fa46c79f7e407
Ramiro Augusto Cosentino <rama@xicnet.com>
2015-04-16 22:32:36 -0300

    Add customized app interface for Telam

---
+++ customized/telam_www/js/config.js
@@ -0,0 +1,11 @@
+var contentServer = "http://rnadmin.xicnet.com";
+var streamURL     = "http://icecast01.dcarsat.com.ar:8000/sc_rad1";
+
+// Make config variables global
+window.server = contentServer;
+window.streamURL = streamURL;
+window.facebook = "fb://profile/161243707242002";
+window.twitter = "AgenciaTelam";
+window.web = "http://www.telam.com.ar/";
+window.email = "contacto@telam.com.ar";
+window.isPlaying = false;
